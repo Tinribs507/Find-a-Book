b32c75b9997b49be9231a936ad974f8394d4e6ba
Tinribs507 <50043792+Tinribs507@users.noreply.github.com>
2024-11-16 08:35:30 +0000

    Removed redundant code

--- app/src/main/java/com/example/findabook/displays/results/components/BookCard.kt
+++
@@ -1,9 +0,0 @@
-package com.example.findabook.displays.results.components
-
-import androidx.compose.runtime.Composable
-import com.example.findabook.data.model.Book
-
-@Composable
-fun BookCard(book: Book) {
-
-}
