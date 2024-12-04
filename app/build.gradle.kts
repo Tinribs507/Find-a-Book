f72e617d7f1b4bd94c1a60df395971ab34df5e9d
Tinribs507 <50043792+Tinribs507@users.noreply.github.com>
2024-12-04 18:02:31 +0000

    feat: Added Coil so the app can display images from URLS
    - Added Coil implementation to 'build.gradle.kt'
    - Added coil version 2.6.0 to 'libs.versions.toml'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.runtime.livedata)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui.text.google.fonts)
+
     testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.mockito.core)
@@ -69,4 +70,7 @@ dependencies {
 
     // Moshi
     api(libs.bundles.moshi)
+
+    // Coil
+    implementation(libs.coil.compose)
 }
